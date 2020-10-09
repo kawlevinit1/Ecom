9cf2ae139dad49eed0243471912a354b99375a5a
kawlevinit1 <kawlevinit@gmail.com>
2020-10-09 17:17:56 +0530

    changes in settings

--- ecom/urls.py
+++ ecom/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('product.urls')),
     path('',include('login.urls')),
+    path('',include('store.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
